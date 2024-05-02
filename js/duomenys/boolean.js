f685be6ab9263211df480df68359120d8fcbdce8
JuliusBTI <j.baksinskas1988@gmail.com>
2024-05-02 13:18:54 +0300

    boolean

---
+++ js/duomenys/boolean.js
@@ -0,0 +1,70 @@
+/*
+BOOLEAN - logines reiksmes
+
+- true
+- false
+
+Boolean logikos operatoriai:
+- && (and)
+- || (or)
+- ! (not)
+*/
+
+const username = 'Jonas';
+const age = 99;
+const isSunShining = true;
+const hasFuel = false;
+
+console.log(isSunShining);
+console.log(hasFuel);
+
+console.log('KAIP NENAUDOTI:', true + true);
+console.log('KAIP NENAUDOTI:', true * false);
+
+console.clear();
+
+console.log('--------------------');
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.clear();
+console.log(true && true || true);
+console.log(true && true || false);
+
+console.log(true && false || false);
+console.log(false && false || false);
+
+console.log(false && true || true);
+console.log(false && true || false);
+
+console.log(false && false || true);
+console.log(false && false || false);
+
+console.log('-----------------');
+
+console.log(true || true && true);
+console.log(true || true && false);
+
+console.log(true || false && false);
+console.log(false || false && false);
+
+console.log(false || true && true);
+console.log(false || true && false);
+
+console.log(false || false && true);
+console.log(false || false && false);
+
+console.log('---------------');
+
+console.log(true && true && true && true);
+console.log(true && true && true && false);
+
+console.log(true && true && true || true);
+console.log(true && true && true || false);
